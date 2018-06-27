c8d6acafe76611d8174701ee4254ab8c7ed5eed7
Allan James Lapid <ajLapid718@gmail.com>
2018-06-27 11:38:32 -0400

    Why no work

--- index.js
+++ index.js
@@ -6,29 +6,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection', function(socket) {
-//   console.log('a user connected');
-//   socket.on('disconnect', function() {
-//     console.log('user disconnected!');
-//   });
-// });
-
-// Comment this out;
-// io.on('connection', function(socket) {
-//   socket.on('chat message', function(msg) {
-//     console.log('message: ' + msg);
-//   });
-// });
-
-// Emit(): Send an event to everyone;
-// io.emit('some event', { for: 'everyone' });
-
-// Broadcast(): Send a message to everyone except for a certain socket;
-// io.on('connection', function(socket){
-//   socket.broadcast.emit('hi');
-// });
-
-// Send a message to everyone, including the sender;
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
