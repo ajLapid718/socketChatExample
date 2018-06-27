6ef81407eb75c693fdf9adedd9d269fd037e88ea
Allan James Lapid <ajLapid718@gmail.com>
2018-06-27 11:21:02 -0400

    Send messages

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// io.on('connection', function(socket) {
+//   console.log('a user connected');
+//   socket.on('disconnect', function() {
+//     console.log('user disconnected!');
+//   });
+// });
+
 io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('user disconnected!');
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
   });
 });
 
